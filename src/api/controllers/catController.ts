65c9cae3c8bcd54be13bcaf2919322eb01f5c120
Anish Maharjan <anishmhn@hotmail.com>
2024-04-02 13:19:04 +0300

    remove todos

--- src/api/controllers/catController.ts
+++ src/api/controllers/catController.ts
@@ -1,14 +1,3 @@
-// TODO: create following functions:
-// - catGetByUser - get all cats by current user id
-// - catGetByBoundingBox - get all cats by bounding box coordinates (getJSON)
-// - catPutAdmin - only admin can change cat owner
-// - catDeleteAdmin - only admin can delete cat
-// - catDelete - only owner can delete cat
-// - catPut - only owner can update cat
-// - catGet - get cat by id
-// - catListGet - get all cats
-// - catPost - create new cat
-
 import {NextFunction, Request, Response} from 'express';
 import {Cat, CatTest, LoginUser, UserOutput} from '../../types/DBTypes';
 import {MessageResponse} from '../../types/MessageTypes';
